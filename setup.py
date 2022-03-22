79a168c83847cc62a76c82a6a2054f0f35e96f84
Py1000 <89375743+Py1000@users.noreply.github.com>
2022-03-22 18:27:14 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='fotpy',
+    version='0.0.1',
+    license='MIT',
+    author='JVK',
+    author_email='@jarvisuserbot@gmail.com',
+    description='fotpy is a file oprator tool for python(low codes) made for personal use',
+    long_description=open('README.md').read(),
+    url='https://github.com/tools1527/fotpy',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
